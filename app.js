f0159bc052143731f860e864e0a2ca345c72e958
Ali Almohaya <alialmohaya@gmail.com>
2017-03-25 08:41:26 +0300

    Add out-stream lib and fix some issus in down-stream file

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 /*global require, console*/
 /*esnext: true*/
 
-var http = require('http'),
-    util = require('util'),
-    fs   = require('fs'),
-    downStream= require('./lib/down-stream');
+var http       = require('http'),
+    util       = require('util'),
+    fs         = require('fs'),
+    DownStream = require('./lib/down-stream'),
+    OutStream  = require('./lib/out-stream');
 
-var down = new downStream('./temp/file.mp4');
+var url = 'http://127.0.0.1/player_streaming/outside/Movies/d81xgm-nY8RygfSuWLKCzC8kvSzlXZv9xDq0vkxYOYQ8_RwlZxClIPnNy5cr_qc5DCnoouS7_EJTVt6EXIjM74DHIQjukdjawkC30s7Kl5jbR6jtnOdwfxUuKSppEic_yISnSvOhN_xJVlVDqVmcY1IeHTO2axlH_14giD9Vpi3b4qQXtm0xy3f1pPP5X9Fy';
+
+var fileName = './temp/letItGo.mp4';
 
 
-down.start('http://localhost:3000/movie.mp4', function (err, nameFile) {
-    
+var OS = new OutStream(http);
+
+
+var down = new DownStream('./temp/file.mp4');
+
+down.start(url, function (err, streamFile) {
+    'use strict';
     if (err) {
         console.log(err.message);
     }
     
-    fs.unlink(nameFile, function () {
+    if (streamFile) {
         
-        console.log('File deleted');
+        OS.createServer(streamFile, '/stream');
+        OS.listen(3000);
         
-    });
+    }
     
 });
-
