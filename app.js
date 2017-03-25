9d7ed70dbf08b2912507c42a7f4cf9187bc21e49
Ali Almohaya <alialmohaya@gmail.com>
2017-03-25 07:35:52 +0300

    Fix the wirte streaming and add new dir for temp files

--- app.js
+++ app.js
@@ -4,6 +4,22 @@
 var http = require('http'),
     util = require('util'),
     fs   = require('fs'),
-    down = require('./lib/down-stream');
+    downStream= require('./lib/down-stream');
 
-var xx = new down();
+var down = new downStream('./temp/file.mp4');
+
+
+down.start('http://localhost:3000/movie.mp4', function (err, nameFile) {
+    
+    if (err) {
+        console.log(err.message);
+    }
+    
+    fs.unlink(nameFile, function () {
+        
+        console.log('File deleted');
+        
+    });
+    
+});
+
